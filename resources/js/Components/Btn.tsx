cb2d5cbbc5d7c0eb5f3c00e50036647bd6d063e4
salah <cloud.data.sa@gmail.com>
2024-10-24 19:01:23 -0700

    dsadA

--- resources/js/Components/Btn.tsx
+++ resources/js/Components/Btn.tsx
@@ -6,6 +6,7 @@ const Btn = ({children,className,a='bg-gray-100',b='bg-gray-50',onClick,disabled
     const [aa,stA] = useState(false)
     return <div onClick={onClick} onMouseDown={()=>stA(true)} onMouseUp={()=>stA(false)}
                    className={cn(
+                       'cursor-pointer',
                        `${aa?`${b} translate-y-[1px]`:`${a} `}`,
                        rounded?'rounded-full flex justify-center items-center  text-center':'',
                        `hover:${b}`,
