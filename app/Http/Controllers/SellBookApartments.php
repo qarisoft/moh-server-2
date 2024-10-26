72bafe94c2c75de191a1b44876bc5b183813cdce
qarisoft <131317208+qarisoft@users.noreply.github.com>
2024-10-27 00:22:02 +0300

    update package.json

--- app/Http/Controllers/SellBookApartments.php
+++ app/Http/Controllers/SellBookApartments.php
@@ -4,50 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Models\Apartment;
 use App\Models\Customer;
-use App\Models\Operation;
 use App\Models\Project;
-use Faker\Core\File;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-use Inertia\Inertia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class SellBookApartments extends Controller
 {
-    function sell(Apartment $apartment,Request $request)
+    function sell(Apartment $apartment, Request $request)
     {
 
-        $request->validate([
-            'customer_id'=>'required|string|not_in:0',
-            'apartment_id'=>'required|int',
-
-        ]);
-        $apartment = Apartment::find($request->apartment_id);
-
-        $apartment->sell($request->customer_id);
+        if ($request->create_customer == '1') {
+            $request->validate([
+                'name' => 'required|string',
+                'phone' => 'required|string',
+                'address' => 'required|string',
+                'apartment_id' => 'required|int',
+            ]);
+            $customer = Customer::create([
+                'name' => $request->name,
+                'address' => $request->address,
+                'phone' => $request->phone,
+            ]);
+            $apartment = Apartment::find($request->apartment_id);
+            $apartment->sell($customer->id);
+        } else {
+            $request->validate([
+                'customer_id' => 'required|string',
+                'apartment_id' => 'required|int',
+            ]);
+            $apartment = Apartment::find($request->apartment_id);
+            $apartment->sell($request->customer_id);
+        }
     }
 
     function book(Request $request)
     {
-        if ($request->create_customer=='1'){
+        if ($request->create_customer == '1') {
             $request->validate([
-                'name'=>'required|string',
-                'phone'=>'required|string',
-                'address'=>'required|string',
-                'apartment_id'=>'required|int',
+                'name' => 'required|string',
+                'phone' => 'required|string',
+                'address' => 'required|string',
+                'apartment_id' => 'required|int',
             ]);
             $customer = Customer::create([
-                'name'=>$request->name,
-                'address'=>$request->address,
-                'phone'=>$request->phone,
+                'name' => $request->name,
+                'address' => $request->address,
+                'phone' => $request->phone,
             ]);
             $apartment = Apartment::find($request->apartment_id);
             $apartment->book($customer->id);
-        }else{
+        } else {
             $request->validate([
-            'customer_id'=>'required|string',
-                'apartment_id'=>'required|int',
+                'customer_id' => 'required|string',
+                'apartment_id' => 'required|int',
             ]);
             $apartment = Apartment::find($request->apartment_id);
             $apartment->book($request->customer_id);
@@ -55,10 +65,10 @@ class SellBookApartments extends Controller
 
     }
 
-    function cancel(Apartment $apartment,Request $request)
+    function cancel(Apartment $apartment, Request $request)
     {
         $request->validate([
-            'apartment_id'=>'required|int',
+            'apartment_id' => 'required|int',
         ]);
         $apartment = Apartment::find($request->apartment_id);
 
@@ -66,27 +76,18 @@ class SellBookApartments extends Controller
     }
 
 
-    public function upload(Request $request,Project $project)
+    public function upload(Request $request, Project $project)
     {
-        // if($request->file('photo')){
+        $request->validate([
+            'photo' => 'required',
+            'project_id' => 'required|int'
+        ]);
 
-        // }
-            $request->validate([
-                'photo' => 'required',
-                'project_id'=>'required|int'
-            ]);
+        $file = $request->file('photo');
 
-            $file = $request->file('photo');
-            // $path = $file->move('uploads', 'public');
-
-            // dd()
-            ini_set('memory_limit', '-1');
-            $project->addMediaFromRequest('photo')->toMediaCollection();
-
-            // $project->addMediaFromDisk($path,'public')->toMediaCollection();
-            // dd($path);
-            // return Inertia::render('Projects/Index');
-            // Project::find($request->id)->addMedia($path)->nonQueued();
+        // dd()
+        ini_set('memory_limit', '-1');
+        $project->addMediaFromRequest('photo')->toMediaCollection();
 
     }
 
--- resources/js/Components/ui/toast.tsx
+++ resources/js/Components/ui/toast.tsx
@@ -13,14 +13,14 @@ const Toaster = ({...props}: ToasterProps) => {
         <Sonner
             theme={theme as ToasterProps["theme"]}
 
-  
+
             className={cn("toaster group", props.className)}
             toastOptions={{
 
                 classNames: {
                     icon: '',
                     toast:
-                        "group  toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg ar",
+                        "group  toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg ",
                     description: "group-[.toast]:text-muted-foreground px-2",
                     actionButton:
                         "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
