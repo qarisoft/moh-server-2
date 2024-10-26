f0425ec4fb72189ac105a0d149fa15da416a34c8
qarisoft <131317208+qarisoft@users.noreply.github.com>
2024-10-26 23:17:24 +0300

    update package.json

--- database/migrations/2024_10_18_181659_create_apartments_table.php
+++ database/migrations/2024_10_18_181659_create_apartments_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Floor::class);
+            $table->foreignIdFor(Floor::class)->constrained()->onDelete('cascade');
 //            $table->foreignIdFor(Floor::class);
             $table->boolean('is_booked')->default(false);
             $table->boolean('is_soled')->default(false);
